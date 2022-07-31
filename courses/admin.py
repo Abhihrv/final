c80ca5c76ba7a9e7d7cfcb234a876a8bd3fb5482
Abhijay Sharma <abhihrv@gmail.com>
2022-07-31 12:37:44 -0600

    creating course app admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
+
+class DegreeAdmin(admin.ModelAdmin):
+    list_display = ('dept','code', 'name', 'credit_required')
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('dept','code', 'name', 'credits', 'degree')
+
+class StudentCourseAdmin(admin.ModelAdmin):
+    list_display = ('student','course', 'grade', 'status')
+
+class StudentDegreeAdmin(admin.ModelAdmin):
+    list_display = ('student','degree', 'credits_achieved', 'status')
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Degree, DegreeAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(StudentCourse, StudentCourseAdmin)
+admin.site.register(StudentDegree, StudentDegreeAdmin)
+
